09faf5ef3f5d6c8c9e8c4211369606c17644ba29
singha4 <amandeep159632478@gmail.com>
2014-03-25 10:09:04 -0400

    version 1

---
+++ ChapterFourProblems/GetVowel/GetVowel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetVowel
+{
+    class GetVowel
+    {
+        static void Main(string[] args)
+        {
+
+            int sum = 0;
+            string input;
+            int vowel;
+            while (true)
+            {
+                Console.WriteLine("Enter a vowel");
+                input = Console.ReadLine();
+
+                if (input == "a" || input == "e" || input == "i" || input == "o" || input == "u")
+                {
+                    Console.WriteLine("OK");
+                }
+            }
+        }
+    }
+}
---
+++ ChapterFourProblems/Sum/Sum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sum
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string input;
+            int n;
+            while(true)
+            {
+                Console.WriteLine("Enter a number");
+                input = Console.ReadLine();
+                n = Convert.ToInt32(input);
+
+                if (input == "999")
+                {
+                    break;
+                }
+
+                if (n < 999)
+                {
+                    sum += n;
+                }
+                Console.WriteLine("The sum is {0}",sum);
+            }
+        }
+    }
+}
